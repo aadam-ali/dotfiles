488fd1a0a09bea6890daa98c32035729e2784cb3
Aadam Ali <57071686+Aadam-Ali@users.noreply.github.com>
2024-01-04 21:53:42 +0000

    Polybar multi-monitor support

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -4,6 +4,12 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar example &
+non_primary_monitors=$(xrandr -q | grep -w 'connected' | grep -v 'primary' | cut -d' ' -f1)
+
+polybar --config="$HOME/.config/polybar/config.ini" primary &
+
+for m in $non_primary_monitors; do
+  MONITOR=$m polybar --config="$HOME/.config/polybar/config.ini" secondary &
+done
 
 echo "Polybar launched..."
