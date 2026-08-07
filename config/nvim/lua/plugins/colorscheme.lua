74c25e8ab230e21d3197d76a8d09fe9b77a0dc7b
Aadam Ali <57071686+aadam-ali@users.noreply.github.com>
2026-08-07 23:40:51 +0100

    style: Use Gruvbox Hard (not material)
    This has a higher contrast, making it more friendly when sharing my
    screen, or working in environments with bad lighting.

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,8 @@ return {
   "sainnhe/gruvbox-material",
   config = function()
     vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_foreground = "original"
+    vim.g.gruvbox_material_background = "hard"
     vim.opt.background = "dark"
     vim.cmd("colorscheme gruvbox-material")
   end,
